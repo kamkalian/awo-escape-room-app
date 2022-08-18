527dae5cb3827b972bb9d0b25b74695ba1adc83f
kamkalian <kurm_de+gitlab@pm.me>
2022-08-18 15:23:29 +0200

    First try with SerialPort

---
+++ pages/api/status.ts
@@ -0,0 +1,46 @@
+import { SerialPort } from 'serialport'
+import { ReadlineParser } from 'serialport'
+import { accessSync, constants } from 'node:fs';
+
+
+var data: String;
+var fs = require('fs'); 
+
+
+export default function handler(req: any, res: any){
+    console.log("################## api status call")
+
+    function sendData(code: any, msg: string) {
+        console.log("sendData")
+        console.log(msg);
+        res.status(code).json({data: msg});
+        res.end();
+    }
+
+    var port = new SerialPort({
+        path: '/dev/ttyUSB0',
+        baudRate: 9600}, 
+        function (err) {
+            if (err) {
+              return console.log('Error: ', err.message)
+            }
+        }
+    )
+
+    var buffer: any = '';
+    port.on('data', function(chunk: string) {
+        buffer += chunk;
+        var answers = buffer.split(/\r?\n/);
+        buffer = answers.pop();
+
+        console.log(buffer)
+        if (answers.length > 0) {
+            sendData(200, answers[0])
+            port.close();
+        }
+    })
+       
+    port.on('error', function(err) {
+        sendData(500, err.message);
+    });
+}
