87b60354b11ca2b17aef5c4c16437ce0a62f60f1
kamkalian <kurm_de+gitlab@pm.me>
2022-09-20 17:21:33 +0200

    Get and show seconds from Serialport

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,19 +2,47 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import React, { useState, useEffect } from 'react'
+import Chip from '@mui/material/Chip';
+import Grid from '@mui/material/Grid';
+
+
 
 
 const Home: any = () => {
-  const res = fetch('/api/status')
-  //const data = res.json()
-  //console.log(data);
-  fetch('/api/profile-data')
-    .then((res) => res.json())
-    .then((data) => {
-      console.log(data);
-    })
+  const [timer, setTimer] = useState(null)
+  const [isLoading, setLoading] = useState(false)
+
+  useEffect(() => {
+    // declare the data fetching function
+    const fetchData = async () => {
+      const resp = await fetch('/api/status/');
+      const json = await resp.json();
+      setTimer(json.data);
+    }
+  
+    // call the function
+    const id = setInterval(() => {
+      fetchData()
+        // make sure to catch any error
+        .catch(console.error);
+    }, 1000);
+    return () => clearInterval(id);
+  }, [])
+
   return (
-    <div>test</div>
+    
+    <Grid container spacing={2}>
+      <Grid item xs={12}>
+        <h1>{timer}</h1>
+      </Grid>
+      <Grid item xs={4}>
+      </Grid>
+      <Grid item xs={4}>
+      </Grid>
+      <Grid item xs={8}>
+      </Grid>
+    </Grid>
   )
 }
 
